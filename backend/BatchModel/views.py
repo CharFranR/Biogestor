df6eca4ee0dc631734c0630fee30e5d0d8cde0b1
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2025-11-26 21:35:34 -0600

    fix(BatchModel): user correct serializer format and delete unnecesary date_period field

--- backend/BatchModel/views.py
+++ backend/BatchModel/views.py
@@ -11,22 +11,32 @@ class BasicParamsViewSet(viewsets.ModelViewSet):
     serializer_class = BasicParamsSerializer
 
 class mathModelAPI (APIView):
-    def calculate (self, request, format=None):
+    def post (self, request, format=None):
 
-        filling_mass = request.filling_mass
-        approx_density = request.approx_density
-        added_watter = request.added_watter
-        type_material = request.type_material
-        filling_moisture = request.filling_moisture
-        delay_time = request.delay_time
-        date_period = request.date_period
-        temperature = request.temperature
+        filling_mass = request.data['filling_mass']
+        approx_density = request.data['approx_density']
+        added_watter = request.data['added_watter']
+        type_material = request.data['type_material']
+        filling_moisture = request.data['filling_moisture']
+        delay_time = request.data['delay_time']
+        temperature = request.data['temperature']
 
         basic_params = BasicParams.objects.get(supplyName = type_material)
 
         data = simulation (basic_params, filling_mass, filling_moisture, temperature, 
-                           added_watter, approx_density, delay_time, date_period)
+                           added_watter, approx_density, delay_time)
         
-        serializer = BathModelSerializer(data)
+        data_dict = {
+            "total_solids": data[0],
+            "total_volatile_solids": data[1],
+            "potencial_production": data[2],
+            "max_mu": data[3],
+            "solvent_volume": data[4],
+            "initial_concentration": data[5],
+            "specific_mu": data[6],
+            "cumulative_production": data[7],
+        }
 
-        return (serializer.data)
+        serializer = BathModelSerializer(data_dict)
+
+        return Response(serializer.data)
