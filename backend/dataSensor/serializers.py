07af93eae523e1b7128f0d2a21ae9b577ee5f2e8
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2025-11-27 22:33:13 -0600

    feature(dataSensor): serializers

---
+++ backend/dataSensor/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import MeasuredVariable, Sensor
+
+class MeasuredVariableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MeasuredVariable
+        fields = ('id', 'name')
+
+class SensorSerializer(serializers.ModelSerializer):
+    measured_variable = MeasuredVariableSerializer (read_onlye = True)
+    class Meta:
+        model = Sensor
+        field = ('id', 'measured_variable', 'suscription_date', 
+                 'min_range', 'max_range', 'hysteresis', 'accuracy', 'precision')
