38d381cfc2a7e82102fa55e7a3081363ca888b1d
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2025-11-28 23:03:50 -0600

    feature(Fill): models

--- backend/Fill/models.py
+++ backend/Fill/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from BatchModel.models import BasicParams
 
 # Create your models here.
+class Fill (models.Model):
+    first_day = models.DateField (auto_now_add = True)
+    last_day = models.DateField (null = True, blank = True)
+    supply_params = models.ForeignKey(BasicParams, on_delete=models.CASCADE)
+    people_involved = models.TextField(null = True, blank = True)
