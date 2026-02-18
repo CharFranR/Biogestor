b097d72588fb627daad6aef188f66fc408028dc7
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-02-17 20:10:23 -0600

    add (inventario/models): measurement field

--- backend/inventario/models.py
+++ backend/inventario/models.py
@@ -7,5 +7,6 @@ class items (models.Model):
     # Se registran los items en el inventario guardando el nombre, la cantidad del mismo y el area
     # del cidtea a la cual pertenece.
     name = models.CharField(max_length=200)
+    measurement = models.CharField(max_length=200)
     quantity = models.IntegerField()
     place = models.ForeignKey(place, on_delete=models.CASCADE)
